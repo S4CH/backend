47cceff5e9c7783a19d5ab0836c05057356452ca
ayush893 <mauryaayush893@gmail.com>
2021-02-24 19:55:59 +0530

    refactor

--- devsnest/app/controllers/omniauth_callbacks_controller.rb
+++ devsnest/app/controllers/omniauth_callbacks_controller.rb
@@ -1,19 +1,8 @@
 # frozen_string_literal: true
+# This is oauth controller to handle discord_login
 
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
-    user = User.find_for_google_oauth2(request.env['omniauth.auth'], current_user)
-    if user.persisted?
-      sign_in_and_redirect user, event: :authentication
-    else
-      render 'There was an error while trying to authenticate you...'
-    end
-  end
-
   def discord
-    # user = request.env['omniauth.auth']
-    # render json:user
-
     user = User.find_for_discord(request.env['omniauth.auth'], current_user)
     if user.persisted?
       sign_in_and_redirect user, event: :authentication
